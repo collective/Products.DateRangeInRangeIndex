645c9f735bd332b4454bd433cda1d96e7c00db55
Michael Howitz <mh@gocept.com>
2019-03-21 08:19:13 +0100

    Preparing release 2.0.1

--- CHANGES.rst
+++ CHANGES.rst
@@ -2,7 +2,7 @@
 History
 =======
 
-2.0.1 (unreleased)
+2.0.1 (2019-03-21)
 ------------------
 
 - Fix exportimport.py (GenericSetup) for Python 3 and add Python 3.7 to test matrix
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 import os
 
 
-version = '2.1.dev0'
+version = '2.0.1'
 shortdesc = 'Zope index to query a daterange on objects with a daterange'
 longdesc = open(os.path.join(os.path.dirname(__file__), 'README.rst')).read()
 longdesc += open(os.path.join(os.path.dirname(__file__), 'CHANGES.rst')).read()
